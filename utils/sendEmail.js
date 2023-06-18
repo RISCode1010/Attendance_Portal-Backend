96263b41016f9ac3f62989e6daf3ac49af8ed4e9
RISCode1010 <92925907+RISCode1010@users.noreply.github.com>
2023-06-19 00:08:34 +0530

    16 commit

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -20,6 +20,19 @@ const sendEmail = async (val)=>{
         },
         connectionTimeout: 5 * 60 * 1000
     })
+
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
     // console.log("B");
     const mail = {
         from: process.env.EMAIL_FROM,
